d2ba368fa9ef75584a074261db1f231686fabf7a
kk <kk@coderion.pl>
2019-12-17 12:56:36 +0100

    Added using class Euklides

--- src/main/java/pl/coderion/springdemo/controller/MyController.java
+++ src/main/java/pl/coderion/springdemo/controller/MyController.java
@@ -1,7 +1,10 @@
 package pl.coderion.springdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderion.Euklides;
+import pl.coderion.Ulamek;
 
 @RestController
 public class MyController {
@@ -9,5 +12,12 @@ public class MyController {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello world";
+    }
+
+    @GetMapping("/uprosc/{licznik}/{mianownik}")
+    public String uproscUlamek(@PathVariable("licznik") int licznik, @PathVariable("mianownik") int mianownik) {
+
+        Ulamek ulamek = new Ulamek(licznik, mianownik);
+        return Euklides.uprosc(ulamek).toString();
     }
 }
