f24d593b0adaee74c0449f025fef8321aa9aefb8
kk <kk@coderion.pl>
2019-12-17 13:03:49 +0100

    Added request IP address

--- src/main/java/pl/coderion/springdemo/controller/MyController.java
+++ src/main/java/pl/coderion/springdemo/controller/MyController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.coderion.Euklides;
 import pl.coderion.Ulamek;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
 @RestController
 public class MyController {
 
@@ -15,9 +18,12 @@ public class MyController {
     }
 
     @GetMapping("/uprosc/{licznik}/{mianownik}")
-    public String uproscUlamek(@PathVariable("licznik") int licznik, @PathVariable("mianownik") int mianownik) {
+    public String uproscUlamek(@PathVariable("licznik") int licznik,
+                               @PathVariable("mianownik") int mianownik,
+                               HttpServletRequest request) {
 
         Ulamek ulamek = new Ulamek(licznik, mianownik);
+        System.out.println(String.format("[%s] %s %s", new Date(), request.getRemoteAddr(), ulamek));
         return Euklides.uprosc(ulamek).toString();
     }
 }
