8b94a90052441f05781ef3df3b92896bc40dec39
kk <kk@coderion.pl>
2020-02-19 08:42:16 +0100

    Refactroing tests

--- src/test/java/pl/coderion/ulamki/UlamkiTest.java
+++ src/test/java/pl/coderion/ulamki/UlamkiTest.java
@@ -5,6 +5,7 @@ import pl.coderion.Euklides;
 import pl.coderion.Ulamek;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UlamkiTest {
 
@@ -13,6 +14,7 @@ public class UlamkiTest {
         Ulamek ulamek = new Ulamek(12, 18);
         ulamek = Euklides.uprosc(ulamek);
 
+        assertNotNull(ulamek);
         assertEquals(2, ulamek.getLicznik());
         assertEquals(3, ulamek.getMianownik());
     }
