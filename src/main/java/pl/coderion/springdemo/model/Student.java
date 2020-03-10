1f870979316135a1bbdc2ae7e0a201abc87e224b
kk <kk@coderion.pl>
2020-03-10 11:06:25 +0100

    Added lombok

--- src/main/java/pl/coderion/springdemo/model/Student.java
+++ src/main/java/pl/coderion/springdemo/model/Student.java
@@ -1,7 +1,10 @@
 package pl.coderion.springdemo.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "tbl_student")
 public class Student {
@@ -15,36 +18,4 @@ public class Student {
     private String lastName;
 
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
